73208804d13a1b20c45a28f154316741ad9282e1
Motherbloods <habibskh06@gmail.com>
2025-06-01 20:29:23 +0700

    Refactor: throw errors instead of using res in expense services

--- nodejs/services/expense.service.js
+++ nodejs/services/expense.service.js
@@ -7,23 +7,20 @@ const getExpensesService = async (userId) => {
     throw new Error("Error fetching expenses");
   }
 };
+
 const createExpenseService = async (data, userId) => {
   const { name, amount, category, date } = data;
 
   if (!name || !amount || !category || !date) {
-    return res.status(400).json({
-      success: false,
-      message:
-        "Please provide all the required fields: name, amount, category, and date.",
-    });
+    throw new Error(
+      "Missing required fields: name, amount, category, and date."
+    );
   }
 
   if (isNaN(amount)) {
-    return res.status(400).json({
-      success: false,
-      message: "Amount must be a number.",
-    });
+    throw new Error("Amount must be a number.");
   }
+
   const newExpense = new ExpenseTracker({
     userId: userId,
     name,
@@ -31,24 +28,20 @@ const createExpenseService = async (data, userId) => {
     category,
     date,
   });
+
   return await newExpense.save();
 };
+
 const editExpenseService = async (data, expenseId, userId) => {
   const { name, amount, category, date } = data;
   const expense = await ExpenseTracker.findById(expenseId);
 
   if (!expense) {
-    return res.status(404).json({
-      success: false,
-      message: "Expense not found",
-    });
+    throw new Error("Expense not found.");
   }
 
   if (expense.userId.toString() !== userId.toString()) {
-    return res.status(403).json({
-      success: false,
-      message: "You are not authorized to edit this expense.",
-    });
+    throw new Error("You are not authorized to edit this expense.");
   }
 
   return await ExpenseTracker.findByIdAndUpdate(
@@ -63,8 +56,10 @@ const editExpenseService = async (data, expenseId, userId) => {
     { new: true }
   );
 };
+
 const deleteExpenseService = async (expenseId, userId) => {
   const expense = await ExpenseTracker.findOne({ _id: expenseId, userId });
+
   if (!expense) {
     throw new Error("Expense not found or you're not authorized to delete it.");
   }
