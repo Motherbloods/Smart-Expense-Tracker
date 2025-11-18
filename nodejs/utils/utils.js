0b0efe89886a2adf614fda0a855509cb2408b212
Motherbloods <habibskh06@gmail.com>
2025-11-18 09:52:02 +0700

    feat: add localhost to CORS allowed origins

--- nodejs/utils/utils.js
+++ nodejs/utils/utils.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 const corsOptions = {
-  origin: process.env.URL_FRONT_END, // Your frontend URL
+  origin: [process.env.URL_FRONT_END, "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
